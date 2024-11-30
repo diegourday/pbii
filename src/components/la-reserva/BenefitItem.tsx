c3b7440276a582a923b5b48fc6a593187c29a8f2
diegourday <dh7586@outlook.com>
2024-11-29 20:41:42 -0500

    feat: enhance styling of BenefitItem component with rounded corners and improved hover effects

--- src/components/la-reserva/BenefitItem.tsx
+++ src/components/la-reserva/BenefitItem.tsx
@@ -8,10 +8,10 @@ const BenefitItem: FC<BenefitItemProps> = ({
   return (
     <div
       onClick={onSelect}
-      className="group/item flex cursor-pointer flex-col items-center px-6 py-7 transition-all hover:scale-105 hover:shadow-md hover:shadow-primary/10"
+      className="group/item flex cursor-pointer flex-col items-center rounded-md px-6 py-7 transition-all hover:scale-105 hover:shadow-md hover:shadow-primary/10"
     >
       <div className="mb-2 rounded-full bg-slate-50 p-3 transition-colors group-hover/item:bg-slate-100">
-        <Icon className="group-hover/item:animate-stroke size-10 stroke-1 text-primary" />
+        <Icon className="size-10 stroke-1 text-primary group-hover/item:animate-stroke" />
       </div>
       <h3 className="mb-1 font-medium text-primary">{title}</h3>
       <p
